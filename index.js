8278daf5267533bec71b7e7edf13c7c3701ff86c
Oliver <oliver.grant@gmail.com>
2018-12-18 01:48:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function placeOrder(cardNumber) {
   // write your code here
   var totCost=""
   if (cardNumber) {
-    totCost+="Your total cost is $"+total()+", which will be charged to the card "+cardNumber
+    totCost+="Your total cost is $"+total()+", which will be charged to the card "+cardNumber+"."
     console.log(totCost)
     cart.splice(0,cart.length)
   } else return "Sorry, we don't have a credit card on file for you."
