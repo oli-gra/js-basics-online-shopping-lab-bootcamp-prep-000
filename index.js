057eb3d7cb0661135c4e6416f1a3016ce88549c1
Oliver <oliver.grant@gmail.com>
2018-12-18 01:43:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cardNumber>) {
+  if (cardNumber) {
     "Your total cost is $" + total() + `, which will be charged to the card ${cardNumber}.`
     cart.splice(0,cart.length)
   } else return ("Sorry, we don't have a credit card on file for you.")
