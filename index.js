9c56a8205752fe8e0191610cdde97c8f067b3dd1
Oliver <oliver.grant@gmail.com>
2018-12-18 01:24:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,14 +48,16 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   var i=0,n=cart.length,foundItem=false
-  for (i=0;i==n;i++) {
-    if (item==cart[i].itemName) {
+  while (foundItem===false&&i<n) {
+    i++
+    if (item==cart[i].itemName)
+    {
       foundItem=true
       cart.splice(i,1)
       return cart
     }
   }
-  if (i==n&&foundItem) return "That item is not in your cart."
+  if (foundItem===false) return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
