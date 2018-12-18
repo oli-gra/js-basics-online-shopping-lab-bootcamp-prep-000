ffa91e8b8ee592c4afe94b7060a40f1cddd2f8ac
Oliver <oliver.grant@gmail.com>
2018-12-18 01:18:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,6 +62,6 @@ function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber>0) {
     return "Your total cost is $" + total() + `, which will be charged to the card ${cardNumber}.`
-    cart=[]; // does not clear the cart array
+    cart.length=0
   } return "Sorry, we don't have a credit card on file for you."
 }
