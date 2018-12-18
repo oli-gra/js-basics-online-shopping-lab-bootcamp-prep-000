25575e374e97b47c72325d7a33b305b38c197793
Oliver <oliver.grant@gmail.com>
2018-12-18 01:56:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,10 +63,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  var totCost=""
+  var cardTotal=total()
   if (cardNumber) {
-    totCost+="Your total cost is $"+total()+", which will be charged to the card "+cardNumber+"."
-    console.log(totCost)
     cart.splice(0,cart.length)
-  } else console.log("Sorry, we don't have a credit card on file for you."
+    return `Your total cost is ${total}, which will be charged to the card ${cardNumber}.`
+  } else console.log("Sorry, we don't have a credit card on file for you.")
 }
