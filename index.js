91676aa2baca71a2ca904329347ca9878cdf2d9f
Oliver <oliver.grant@gmail.com>
2018-12-18 00:58:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ function addToCart(item) {
 function viewCart() {
   var i,n=cart.length,itemsincart=""
   function puncitem(i) {
-    if (n-i=0) return "."
-    if (n-i=1) return ",and "
+    if (n-i===0) return "."
+    if (n-i==1) return ",and "
     if (n-i>1) return ", "
   }
   if (n===0) {
