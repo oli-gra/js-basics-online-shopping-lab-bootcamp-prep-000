f56bb421b14a98e06d1eed14b80777e9d5aa38b4
Oliver <oliver.grant@gmail.com>
2018-12-18 01:39:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,5 +66,5 @@ function placeOrder(cardNumber) {
   if (cardNumber>0) {
     console.log ("Your total cost is $" + total() + `, which will be charged to the card ${cardNumber}.`)
     cart.splice(0,cart.length)
-  } else console.log ("Sorry, we don't have a credit card on file for you.")
+  } else return ("Sorry, we don't have a credit card on file for you.")
 }
